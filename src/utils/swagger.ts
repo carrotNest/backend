6cd8e3fc8a25cd1070f550fb7537f6ff1cb5d9c0
ppinkypeach <mira20932@gmail.com>
2024-03-31 02:59:15 +0900

    fix: swagger에 bearer 토큰 추가

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,22 +1,13 @@
-// import { INestApplication } from '@nestjs/common';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-// export function setupSwagger(app: INestApplication): void {
-//   const options = new DocumentBuilder()
-//     .setTitle('Techeer Auction')
-//     .setDescription('중고 경매 사이트')
-//     .setVersion('1.0.0')
-//     .addBearerAuth(
-//       {
-//         type: 'http',
-//         scheme: 'bearer',
-//         name: 'JWT',
-//         description: 'Enter JWT token',
-//         in: 'header',
-//       },
-//       'token',
-//     )
-//     .build();
-//   const document = SwaggerModule.createDocument(app, options);
-//   SwaggerModule.setup('api', app, document);
-// }
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Techeer Auction')
+    .setDescription('중고 경매 사이트')
+    .setVersion('1.0.0')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
