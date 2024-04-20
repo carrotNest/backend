7cb093eb4a5d28971614d35664f6398b5b8fc445
ppinkypeach <mira20932@gmail.com>
2024-04-20 21:16:54 +0900

    refactor: likes column에 명시적으로 외래키컬럼 설정

--- src/modules/likes/entity/likes.entity.ts
+++ src/modules/likes/entity/likes.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../../user/entity/user.entity";
 import { Board } from "../../board/entity/board.entity";
 
@@ -7,6 +7,12 @@ export class Likes{
 
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({type: 'int', name: 'user_id'})
+    userId: number;
+
+    @Column({type: 'int', name: 'board_id'})
+    boardId: number;
 
     @ManyToOne(type => User)
     @JoinColumn({name: 'user_id'})
