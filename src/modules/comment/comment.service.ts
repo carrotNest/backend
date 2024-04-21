37a5f5badedb89a3efb3806049107cbab444617c
ppinkypeach <mira20932@gmail.com>
2024-04-21 20:23:05 +0900

    refactor: 댓글 생성 api response 변경

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -3,12 +3,12 @@ import { CommentMapper } from './mapper/comment.mapper';
 import { Comment } from './entity/comment.entity';
 import { CommentCursorOptionsDto } from './dto/commet-cursor-options.dto';
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { UserCreateResultInterface } from 'src/interfaces/user-create-result.interface';
 import { PaginationResponseDto } from 'src/global/common/paginate/dto/pagination-response.dto';
 import { CursorPageMetaDto } from 'src/global/common/paginate/dto/cursor-paginate/cursor-page-meta.dto';
 import { CommentRepository } from './repository/comment.repository';
 import { BoardRepository } from '../board/repository/board.repository';
 import { UserRepository } from '../user/repository/user.repository';
+import { GetCommentDto } from './dto/get-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -21,17 +21,19 @@ export class CommentService {
         private readonly commentMapper: CommentMapper
     ) {}
 
-    async createComment(createCommentDto: CreateCommentDto, id: number): Promise<UserCreateResultInterface> {
+    async createComment(createCommentDto: CreateCommentDto, id: number): Promise<GetCommentDto> {
+        const {content} = createCommentDto;
+
         const creator = await this.userRepository.findOneBy({id});
         const board = await this.boardRepository.findOneBy({id: createCommentDto.boardId});
         const newCommentEntity = this.commentMapper.dtoToEntity(createCommentDto,creator,board);
 
         const savedComment = await this.commentRepository.saveComment(newCommentEntity);
 
-        return{
-            message: '새로운 댓글 생성',
-            userId: savedComment.creator.id
-        };
+        const creatorNickname = creator.nickname;
+        const resposne = new GetCommentDto(savedComment.id, savedComment.createAt, content, creatorNickname);
+
+        return resposne;
     }
 
     async getAllComment(commentCursorOptionsDto: CommentCursorOptionsDto): Promise<PaginationResponseDto<Comment>> {
